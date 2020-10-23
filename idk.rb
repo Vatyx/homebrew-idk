24bc2b4507a4570452f659e7bfafb3a914ecd8b1
Sahil Dhanju <Vatyx@users.noreply.github.com>
2020-10-23 22:51:35 +0100

    Update idk.rb

--- idk.rb
+++ idk.rb
@@ -2,14 +2,10 @@ class Idk < Formula
     desc "diff tool with browser-based GUI"
     homepage "https://github.com/Vatyx/idk"
     url "https://api.github.com/Vatyx/idk/releases/latest/idk"
+    version "0.01"
   
     bottle :unneeded
   
     def install
-      bin.install "delta"
-    end
-  
-    test do
-      system "#{bin}/idk", "--version"
     end
   end
