8acfd943dba6c8ce5271312a13caca6651937656
Sahil Dhanju <Vatyx@users.noreply.github.com>
2020-10-24 19:02:58 +0100

    Update idk.rb

--- idk.rb
+++ idk.rb
@@ -1,7 +1,7 @@
 class Idk < Formula
     desc "diff tool with browser-based GUI"
     homepage "https://github.com/Vatyx/idk"
-    url "https://github.com/Vatyx/idk/releases/download/v0.18/idk.zip"
+    url "https://github.com/Vatyx/idk/releases/download/v0.19/idk.zip"
     version "0.18"
   
     bottle :unneeded
