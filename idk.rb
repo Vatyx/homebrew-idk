8aa95fd5b363f426d362f3300ac9d2b130774702
Vatyx <sahildhanju@improbable.io>
2020-10-25 17:42:27 +0000

    Update from https://github.com/Vatyx/idk/commit/2f0eceaa836607d0466bf7d8e54565d6b2c5bd20

--- idk.rb
+++ idk.rb
@@ -1,9 +1,9 @@
 class Idk < Formula
     desc "diff tool with browser-based GUI"
     homepage "https://github.com/Vatyx/idk"
-    url "https://github.com/Vatyx/idk/releases/download/v0.{version}/idk.zip"
-    version "0.{version}"
-    sha "{checksum}"
+    url "https://github.com/Vatyx/idk/releases/download/v0.28/idk.zip"
+    version "0.28"
+    sha "086e92cab2b1bb60a64d86bf7ed3ff11b676d19e49282d46e839c2c637f3721f"
   
     bottle :unneeded
   
